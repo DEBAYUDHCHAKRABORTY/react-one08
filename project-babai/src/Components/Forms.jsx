5f4a45a7ccf0f91ff1d47d0704dfde1b3d209e2e
Nabin94 <Nabinkrmisra94@gmail.com>
2025-09-25 10:02:53 +0530

    forms added

--- project-babai/src/Components/Forms.jsx
+++ project-babai/src/Components/Forms.jsx
@@ -1,12 +1,37 @@
-export default function Forms (){
+import { useState } from "react";
 
-return (
-<div>
-    <h1>this is a react form</h1>
-    
+export default function Forms() {
 
-</div>
+    const [name, setName] = useState();
+    const [gender, setGender] = useState();
 
-)
+    function handleInputChange(e) {
+        const inpKey = e.target.name;
+        const inpVal = e.target.value;
 
-}
+        if (inpKey === "userInput") {
+            setName(inpVal);
+        } else {
+            setGender(inpVal);
+        }
+    }
+
+
+    return (
+        <div className="container">
+            <h1>This is a react form</h1>
+
+            <input type="text" name="userInput" placeholder="enter your name" onInput={handleInputChange} />
+            <div>
+                <input type="radio" value="male" onChange={handleInputChange} name="Gender" /> Male
+                <input type="radio" value="female" onChange={handleInputChange} name="Gender" /> Female
+                <input type="radio" value="others" onChange={handleInputChange} name="Gender" /> Others
+            </div>
+            {name && <h3>The name is {name}</h3>} 
+            {gender && <h3>Gender is {gender}</h3>}
+
+        </div>
+
+    );
+
+}   
