670807994f94e0a1c2423c127ff02c32b3934bf1
DC <debayudh.maldaitbss@gmail.com>
2025-09-26 21:20:59 +0530

    upgraded

--- czar-react/src/inRadCheck.jsx
+++ czar-react/src/inRadCheck.jsx
@@ -15,38 +15,33 @@ function InRadChe() {
 
     if (inputName === "food") {
       setRad(inputVal);
+      setCheck(inputVal);
     }
 
     if (inputName === "agree") {
-      setCheck(inputVal);
+        const isChecked = event.target.checked
+        if(isChecked){
+            setCheck(rad)
+        }else{
+            setCheck()
+        }
     }
   }
 
   return (
     <div>
-      <input
-        type="text"
-        name="myName"
-        placeholder="Enter your Name"
-        onInput={handleBtn}
-      />
+      <input type="text" name="myName" placeholder="Enter your Name" onInput={handleBtn}/>
 
-      <br />
+      <div>
+        <input type="radio" name="food" value="Chicken" onInput={handleBtn} />Chicken
+        <input type="radio" name="food" value="Mutton" onInput={handleBtn} />Mutton
+      </div>
 
-      <input type="radio" name="food" value="Chicken" onInput={handleBtn} />
-      Chicken
-
-      <input type="radio" name="food" value="Mutton" onInput={handleBtn} />
-      Mutton
-
-      <br />
-
-      <input type="checkbox" name="agree" value="agree" onInput={handleBtn} />
-      agrred
+      <input type="checkbox" name="agree" onInput={handleBtn} />Lover
 
       {name && <h3>Your Name is {name}</h3>}
       {rad && <h3>Your fav dish {rad} ? </h3>}
-      {check && <h3>I agree ! I'm a {rad} lover</h3>}
+      {check && <h3>I agree ! I'm a {check} lover</h3>}
 
     </div>
   );
