59b1e0b744574332cb3b95ec026f82582aafd6b5
Pankaj-web861 <pankajpramanik002@gmail.com>
2025-10-07 07:48:33 +0530

    added few files

--- pankaj/src/App.jsx
+++ pankaj/src/App.jsx
@@ -3,8 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Add from './components/comp1'
 import Multi from './components/comp2'
+import RadioCity from '../../czar-react/src/radioCities'
+import InRadChe from './components/Radiocheck'
+import Greet from './components/comp4'
 import './App.css'
 import FFF from './components/comp3'
+import Call from './components/api'
 
 function App() {
 
@@ -13,6 +17,10 @@ function App() {
       <Add/>
       <Multi/>
       <FFF/>
+      <RadioCity/>
+      <InRadChe/>
+      <Greet/>
+      <Call/>
     </>
   )
 }
