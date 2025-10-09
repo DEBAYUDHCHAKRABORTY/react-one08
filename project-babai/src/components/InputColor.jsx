561d60df478e10a4b982a08b61fdce09b2ea5a2e
Nabin94 <Nabinkrmisra94@gmail.com>
2025-10-09 21:46:04 +0530

    updated branch

--- project-babai/src/components/InputColor.jsx
+++ project-babai/src/components/InputColor.jsx
@@ -5,14 +5,7 @@ export default function InputColor() {
     const [TextColor, setTextColor] = useState("purple");
 
 
-    function colorHandle() {
-        setTextColor("green")
-        
-    }
 
-    // function greenHandle (){
-    //     setTextColor("green");
-    // }
 
 
 
@@ -24,8 +17,11 @@ export default function InputColor() {
         <>
             <div>
                 <h1 style={{color: TextColor}}>Good evening Everyone...</h1>
+
                 <button style={{ padding: "20px", width:"85px", backgroundColor: 'red', color: 'white' }}value="red" onClick={()=> setTextColor("red")}> Red </button>
-                <button style={{ padding: "20px", backgroundColor:'green', color:'white' }}value="green" onClick={()=> setTextColor("red")}> Green </button>
+
+
+                <button style={{ padding: "20px", backgroundColor:'green', color:'white' }}value="green" onClick={()=> setTextColor("green")}> Green </button>
 
             </div>
  
