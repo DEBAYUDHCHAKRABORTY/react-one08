b6b0e77b45f8ea784a715a44d3ebcf108486ed6f
DC <debayudh.maldaitbss@gmail.com>
2025-10-15 09:24:44 +0530

    a few more steps remained

--- czar-react/src/App.jsx
+++ czar-react/src/App.jsx
@@ -12,6 +12,7 @@ import HomePage from "./components/homePage";
 import { Routes, Route } from "react-router-dom";
 import SignUp from "./components/signUp&LoadPage/SignUp";
 import LogIn from "./components/login/logIn";
+import Success from "./components/success/success";
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
       <Routes>
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<LogIn />} />
+        <Route path="/success" element={<Success />} />
       </Routes>
     </>
   );
