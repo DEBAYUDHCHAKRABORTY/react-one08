8107984afae316d7fb8e7e9a22e093fa9bafffc2
gurudeb9339 <senguptaargha17@gmail.com>
2025-10-19 13:07:21 +0530

    add to the file

--- react-jishu/src/App.jsx
+++ react-jishu/src/App.jsx
@@ -4,28 +4,28 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Rong from './component/Colour'
 import Emoji from './component/Emoj'
-import Pubg from './component/Game'
+import Pubg from './component/game'
 import SignUp from './component/Sign'
 import Terad from './component/Ret1'
 import { Route, Routes } from 'react-router-dom'
-import Data1 from './component/Data'
+import Data1 from './component/quote'
 function App() {
 
   return (
     <>
 
-    <Routes>
-      <Route path = "/pubg" element= {<Pubg/>} />
-      <Route path = "/signUp" element= {<SignUp />} />
-      <Route path="/rong"element={<Rong/>} />
-      <Route path="/emoji"element={<Emoji/>}/>
-      <Route path='/terad'element={<Terad/>}/>
-      <Route path='/data1'element={<Data1/>}
-      
-      
+      <Routes>
+        <Route path="/" element={<Pubg />} />
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/rong" element={<Rong />} />
+        <Route path="/emoji" element={<Emoji />} />
+        <Route path='/terad' element={<Terad />} />
+        <Route path='/Data1' element={<Data1 />} />
 
 
-    </Routes>
+
+
+      </Routes>
     </>
   )
 }
