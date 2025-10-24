5ccfddb82878a026e47a16f70b560c5de8e6fd05
Nabin Kr Misra <Nabinkrmisra94@gmail.com>
2025-10-24 13:48:56 +0530

    Feature babai (#68)
    * new file added
    
    * files added
    
    * filed added
    
    * project-compo file added
    
    * new file added
    
    * new input file added
    
    * fikes added
    
    * updated branch
    
    * updated files added
    
    * greetings compo added
    
    * changes added
    
    * changes added
    
    * changes added
    
    * changes added
    
    * changes added
    
    * new files added
    
    * new files added
    
    * updated files
    
    * new files added
    
    * Search Prime files added

--- project-babai/src/main.jsx
+++ project-babai/src/main.jsx
@@ -1,9 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
+import "primereact/resources/themes/lara-light-blue/theme.css"
+import "primereact/resources/primereact.min.css"
+import "primeicons/primeicons.css"
+import { PrimeReactProvider } from 'primereact/api';
 import "primereact/resources/themes/arya-orange/theme.css"
 import "primereact/resources/primereact.min.css"
 import "primeicons/primeicons.css"
@@ -12,8 +14,9 @@ import "primeicons/primeicons.css"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  // document.getElementById('root')
   <BrowserRouter>
-    <App />
+    <PrimeReactProvider>
+      <App />
+    </PrimeReactProvider>
   </BrowserRouter>,
 );
