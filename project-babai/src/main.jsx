5b8bfb45b4e34e4ab8c5f81798c5ae8bf760dfd4
Nabin Kr Misra <Nabinkrmisra94@gmail.com>
2025-10-24 13:39:10 +0530

    Feature babai (#66)
    * new file added
    
    * files added
    
    * filed added
    
    * project-compo file added
    
    * new file added
    
    * new input file added
    
    * fikes added
    
    * updated branch
    
    * updated files added
    
    * greetings compo added
    
    * changes added
    
    * changes added
    
    * changes added
    
    * changes added
    
    * changes added
    
    * new files added
    
    * new files added
    
    * updated files

--- project-babai/src/main.jsx
+++ project-babai/src/main.jsx
@@ -4,6 +4,9 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
+import "primereact/resources/themes/arya-orange/theme.css"
+import "primereact/resources/primereact.min.css"
+import "primeicons/primeicons.css"
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
