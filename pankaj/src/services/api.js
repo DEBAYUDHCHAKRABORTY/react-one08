0938b944e0188ac98d1a8b393047cb2ddc391042
Pankaj-web861 <pankajpramanik002@gmail.com>
2025-10-25 10:06:12 +0530

    used prime react

--- pankaj/src/services/api.js
+++ pankaj/src/services/api.js
@@ -8,5 +8,8 @@ export const getAllUsers = () => http.get("/users")
 
 export const deleteUserById = (id) => http.delete(`/users/${id}`)
 export const updateUserById = (id) => http.update(`/users/${id}`)
+export const conform1UserById = (id) => http.conform1(`/users/delete/${id}`)
 
-export const deleteUserById = (id) => http.delete(`/users/${id}`)
+export const searchUser = (userName) => http.get(`/users/search/${userName}`)
+
+
