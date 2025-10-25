4c1cdf10e097dea057595c27caa358b462f8665b
DC74 <debayudh.maldaitbss@gmail.com>
2025-10-25 10:15:24 +0530

    Czar react2 (#72)
    * main ready
    
    * loading
    
    * working on button
    
    * Third Day of API done
    
    * today's session done
    
    * useEffect removed
    
    * routing done
    
    * today's done
    
    * repeated codes removed
    
    * a few more steps remained
    
    * quotes comp done
    
    * quote done
    
    * params done
    
    * quotes done
    
    * css of quotes done
    
    * delete & Add User Done
    
    * service and route done
    
    * primereact & tailwind installed
    
    * diologue day

--- czar-react/src/components/USERS/frontPage.jsx
+++ czar-react/src/components/USERS/frontPage.jsx
@@ -1,14 +1,36 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "primereact/button";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
+import { Toast } from "primereact/toast";
+import { ConfirmDialog, confirmDialog } from "primereact/confirmdialog";
 
 const userUrl = "http://localhost:9090/api/v1/users";
 
 export default function FrntPg() {
   const [data, setData] = useState();
   const navigate = useNavigate();
+
+  const toast = useRef(null);
+
+  const accept = () => {
+    toast.current.show({
+      severity: "warn",
+      summary: "Deleted",
+      detail: "Record Deleted",
+      life: 3000,
+    });
+  };
+
+  const reject = () => {
+    toast.current.show({
+      severity: "warn",
+      summary: "Rejected",
+      detail: "You have rejected",
+      life: 3000,
+    });
+  };
 
   useEffect(() => {
     async function userData() {
@@ -27,6 +49,17 @@ export default function FrntPg() {
   }, []);
 
   const handleDelete = async (id) => {
+
+    confirmDialog({
+      message: "Do you want to delete this record?",
+      header: "Delete Confirmation",
+      icon: "pi pi-info-circle",
+      defaultFocus: "reject",
+      acceptClassName: "p-button-danger",
+      accept,
+      reject,
+    });
+
     try {
       const res = await fetch(`${userUrl}/${id}`, {
         method: "DELETE",
@@ -41,6 +74,22 @@ export default function FrntPg() {
     setData(data.filter((data) => data.id !== id));
   };
 
+  const actionBodyTemplate = (rowData) => {
+    return (
+      <>
+        <Toast ref={toast} />
+        <ConfirmDialog />
+        <div className="card flex flex-wrap gap-2 justify-content-center">
+          <Button icon="pi pi-pencil" rounded outlined className="mr-2" />
+          <Button
+            onClick={() => handleDelete(rowData.id)}
+            icon="pi pi-trash"
+          ></Button>
+        </div>
+      </>
+    );
+  };
+
   function handleAddUsers() {
     navigate(`/users/:addUser`);
   }
@@ -48,44 +97,17 @@ export default function FrntPg() {
   return (
     <>
       <div className="flex-col justify-center">
-        <h1 className="text-red-500 text-5xl text-center bg-amber-300">
-          USERS
-        </h1>
+        <h1 className="text-red-500 text-5xl text-center">USERS</h1>
         <Button clas label="ADD USERS" onClick={handleAddUsers} />
         <div className="card bg-red-400">
           <DataTable value={data} tableStyle={{ minWidth: "50rem" }}>
+            <Column header="#" body={(rowData, { rowIndex }) => rowIndex + 1} />
             <Column field="name" header="NAME"></Column>
             <Column field="age" header="AGE"></Column>
             <Column field="city" header="CITY"></Column>
-            {/* <Column field="" header="ACTIONS"></Column> */}
+            <Column body={actionBodyTemplate} header="ACTIONS"></Column>
           </DataTable>
         </div>
-
-        {/* <table>
-              <thead>
-                <tr>
-                  <th>NAME</th>
-                  <th>AGE</th>
-                  <th>PHONE NO.</th>
-                  <th>CITY</th>
-                  <th>ACTIONS</th>
-                </tr> 
-              </thead>
-              <tbody>
-                {data.map((itm, idx) => (
-                  <tr key={idx}>
-                    <td>{itm.name}</td>
-                    <td>{itm.age}</td>
-                    <td>{itm.phone}</td>
-                    <td>{itm.city}</td>
-                    <td>
-                      <Button label="Update" style={{marginRight:'5px'}}/>
-                      <Button label="Delete" onClick={() => handleDelete(itm.id)}/>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table> */}
       </div>
     </>
   );
