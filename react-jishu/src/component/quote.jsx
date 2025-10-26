37f300459239b228c44b48198e231cc086978c33
gurudeb9339 <senguptaargha17@gmail.com>
2025-10-26 13:24:40 +0530

    Poltu (#76)
    * going to pull the api codes from main
    
    * edit to the file
    
    * add to tha file
    
    * add to the file
    
    * caleder added

--- react-jishu/src/component/quote.jsx
+++ react-jishu/src/component/quote.jsx
@@ -18,18 +18,36 @@ export default function Data1() {
     return (
         <div>
             <h1>Data from API</h1>
-            
-            {data && (
-                <>
-                    {data.map((itm, idx) => (
-                        <h2 key={idx}>
-                            <h2>{itm.quote}</h2>
-                            <h2>by {itm.author}</h2>
-                        </h2>
+            <table>
+                <thead>
+                    <tr>
 
-                    ))}
-                </>
-            )}
+                        <th>Id</th>
+                        <th>Quotes </th>
+                        <th>Author</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+
+                    {data && (
+                        <>
+                            <div>
+                                {data.map((itm, idx) => (
+                                    <tr key={idx}>
+                                        <h2>{idx + 1}</h2>
+                                        <h2>{itm.quote}</h2>
+                                        <h2>by {itm.author}</h2>
+                                    </tr>
+
+                                ))}
+                            </div>
+                        </>
+
+                    )
+                    }
+                </tbody>
+            </table>
         </div>
     )
 }
